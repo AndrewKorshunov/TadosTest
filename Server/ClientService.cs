fe046e30349a3d55df7c81fc129828f0e89ba5bf
Andrew <wwrdna@gmail.com>
2016-05-25 13:52:10 +0500

    Made service implementation simple

--- Server/ClientService.cs
+++ Server/ClientService.cs
@@ -2,34 +2,47 @@
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.Configuration;
 using ClientsLibrary;
 
 namespace Server
-{    
+{
     public class ClientService : IClientService
     {
-        public ClientService(IServerLogic logc)
-        {
-        }
+        IDataProvider dataProvider;
 
+        public ClientService()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ClientsDB"].ConnectionString;
+            dataProvider = new DbConnector(connectionString);
+        }
+        
         public bool AddClient(ClientEntity client)
         {
-            return false;
+            int affectedRows = dataProvider.AddClient(client);
+            if (affectedRows == 1) return true;
+            else return false;
         }
 
         public bool EditClient(int updateId, ClientEntity client)
-        {
-            return false;
+        {                
+            int affectedRows = dataProvider.EditClient(updateId, client);
+            // Should affect only 1 client    
+            if (affectedRows == 1) return true;
+            else return false;
         }
 
         public IEnumerable<ClientEntity> GetAllClients()
         {
-            return new[] { new ClientEntity() };
+            return dataProvider.GetAllClients();
         }
 
         public bool RemoveClient(int removeId)
         {
-            return false;
+            int affectedRows = dataProvider.RemoveClient(removeId);
+            // Should affect only 1 client    
+            if (affectedRows == 1) return true;
+            else return false;
         }
     }
 }
